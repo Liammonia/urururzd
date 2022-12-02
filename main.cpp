34ad2b2bb4fa05dbcb12432e1126e2db081cbe04
Liammonia <Alinama03@mail.ru>
2022-12-02 20:18:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-double my_pow(double a, unsigned int b) {
-	double c = a;
-	for (size_t i = 1; i < b; i++)
-		a = a * c;
-	return a;
+double my_pow(double a, int b) {
+double c = a;
+if (b > 0)
+for (size_t i = 1; i < b; i++)
+a *= c;
+else if (b == 0)
+return 1;
+else if (b < 0)
+for (size_t i = 0; i <= abs(b); i++)
+a *= (1 / c);
+
+return a;
 }
